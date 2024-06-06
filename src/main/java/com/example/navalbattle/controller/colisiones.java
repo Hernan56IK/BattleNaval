d81c68b181bef57ccb0fb29aa220af31c1fe2ea8
james bliner m quijano <james.quijano@correounivalle.edu.co>
2024-06-05 23:42:37 -0500

    colisiones creado

---
+++ src/main/java/com/example/navalbattle/controller/colisiones.java
@@ -0,0 +1,10 @@
+package com.example.navalbattle.controller;
+
+public class colisiones {
+    double objeto1;
+    double objeto2;
+    public double suma(double objeto1,double objeto2){
+        return (objeto1+objeto2);
+    }
+
+}
